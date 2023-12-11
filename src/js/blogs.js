614a00400457900f63ef571cea6707a289bd3d61
211omar <omircanov@gmail.com>
2023-12-11 17:20:14 +0400

    CRUD

--- src/js/blogs.js
+++ src/js/blogs.js
@@ -1,7 +1,5 @@
 const urlAPI = "http://localhost:1992/data";
 
-// const blogContent = document.querySelector(".blogs_content");
-
 const blogsContainer = document.querySelector(".blogs .container .block_title");
 const blogContent = document.createElement("div");
 blogContent.classList.add("blogs_content");
@@ -11,6 +9,12 @@ blogsContainer.after(blogContent);
 const newdate = new Date();
 const date1 = newdate.toISOString();
 
+/*more blogs (slice)*/
+
+const moreBlogs = document.querySelector(".all_news");
+moreBlogs.addEventListener("click", () => console.log("all news clicked"));
+
+/* add posts*/
 async function addPosts() {
   try {
     const request = await fetch(urlAPI, {
@@ -33,23 +37,25 @@ async function addPosts() {
   }
 }
 
+/*get posts */
+let x = 3;
 async function getPost() {
   try {
     const request = await fetch(urlAPI);
     const res = await request.json();
     let content = "";
-    res.slice(0,15).forEach((item) => {
+    res.forEach((item) => {
       content = `
       <div class="blog  ">
-      <div class="col_content rounded border border-1 border-dark h-100 ">
-        <div class="news_1_desc p-4">
+      <div class="col_content ">
+        <div class="news_1_desc ">
           <b class='lh-5'>${item.title} </b>
           
-          <div class="news_foot d-flex justify-content-between">
-            <a href="" class="arr_bttn">Daha etrafli</a>
+          <div class="news_foot ">
+            <a href="#" class="arr_bttn">Daha etrafli</a>
             <span class="time">${new Date(item.date).toDateString()} </span>
           </div>
-          <a href="#" class="full_link"></a>
+          <button data-id="${item.id}" class='delete_button'>Delete Post</button>
         </div>
       </div>
     </div>
@@ -66,4 +72,26 @@ async function getPost() {
 }
 getPost();
 
+/*delete post */
+async function deletePost() {
+  try {
+    setTimeout(() => {
+      const deleteBtn = document.querySelectorAll(".delete_button");
+      // console.log(deleteBtn);
+      deleteBtn.forEach((btn) =>
+      btn.addEventListener("click", async function () {
+          console.log(btn)
+          const request = await fetch(`${urlAPI}/${btn.dataset.id}`,{
+            method:"DELETE"
+          });
+          const response = await request.json();
+          console.log(response) ;
+        })
+      );
+    }, 100);
+  } catch (error) {
+    console.log(error + " delete post");
+  }
+}
 
+deletePost();
